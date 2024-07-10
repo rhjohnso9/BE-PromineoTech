a1224242f2fa3cb2acd07e4df4437ae66588bef5
rhjohnso9 <mrjohnson238@hotmail.com>
2024-07-10 10:42:56 -0400

    adding week07MySQL

--- week07mysql-java/src/main/java/projects/exception/DbException.java
+++ week07mysql-java/src/main/java/projects/exception/DbException.java
@@ -7,17 +7,17 @@ public class DbException extends RuntimeException {
 	}	
 	
 	public DbException(String message) {
-		
+		//super(message);
 	}
 	
 	public DbException(Throwable cause) {
-		super(cause);
+		//super(cause);
 		
 	}	
 	
 	
 	public DbException(String message, Throwable cause) {
-		super(message, cause);
+		//super(message, cause);
 		
 	}
 	
